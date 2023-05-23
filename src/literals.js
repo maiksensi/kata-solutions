9bb0908d5a75e8e3d6644cede301fc8a97d2e288
Maik Figura <maik.figura@codecentric.de>
2023-05-23 15:57:56 +0200

    Add mapping solution

--- src/literals.js
+++ src/literals.js
@@ -1,3 +1,26 @@
-export const sum = (a, b) => {
-  return a + b;
+const mapping = [
+  { numeral: 'M', value: 1000 },
+  { numeral: 'CM', value: 900 },
+  { numeral: 'D', value: 500 },
+  { numeral: 'CD', value: 400 },
+  { numeral: 'C', value: 100 },
+  { numeral: 'XC', value: 90 },
+  { numeral: 'L', value: 50 },
+  { numeral: 'XL', value: 40 },
+  { numeral: 'X', value: 10 },
+  { numeral: 'IX', value: 9 },
+  { numeral: 'V', value: 5 },
+  { numeral: 'IV', value: 4 },
+  { numeral: 'I', value: 1 },
+];
+
+export const toRoman = (input) => {
+  let output = '';
+  mapping.forEach((item) => {
+    while (input >= item.value) {
+      output += item.numeral;
+      input -= item.value;
+    }
+  });
+  return output;
 };
--- src/literals.spec.js
+++ src/literals.spec.js
@@ -1,7 +1,76 @@
-import { sum } from './literals';
+import { toRoman } from './literals';
 
-describe('Literals', () => {
-  it('should add 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe('Arabic numbers to roman literals', () => {
+  test.each`
+    input   | expected
+    ${1}    | ${'I'}
+    ${2}    | ${'II'}
+    ${3}    | ${'III'}
+    ${1}    | ${'I'}
+    ${2}    | ${'II'}
+    ${3}    | ${'III'}
+    ${4}    | ${'IV'}
+    ${5}    | ${'V'}
+    ${6}    | ${'VI'}
+    ${7}    | ${'VII'}
+    ${8}    | ${'VIII'}
+    ${9}    | ${'IX'}
+    ${10}   | ${'X'}
+    ${11}   | ${'XI'}
+    ${12}   | ${'XII'}
+    ${13}   | ${'XIII'}
+    ${14}   | ${'XIV'}
+    ${15}   | ${'XV'}
+    ${16}   | ${'XVI'}
+    ${17}   | ${'XVII'}
+    ${18}   | ${'XVIII'}
+    ${19}   | ${'XIX'}
+    ${20}   | ${'XX'}
+    ${21}   | ${'XXI'}
+    ${22}   | ${'XXII'}
+    ${23}   | ${'XXIII'}
+    ${24}   | ${'XXIV'}
+    ${25}   | ${'XXV'}
+    ${26}   | ${'XXVI'}
+    ${27}   | ${'XXVII'}
+    ${28}   | ${'XXVIII'}
+    ${29}   | ${'XXIX'}
+    ${30}   | ${'XXX'}
+    ${31}   | ${'XXXI'}
+    ${32}   | ${'XXXII'}
+    ${33}   | ${'XXXIII'}
+    ${34}   | ${'XXXIV'}
+    ${35}   | ${'XXXV'}
+    ${36}   | ${'XXXVI'}
+    ${37}   | ${'XXXVII'}
+    ${38}   | ${'XXXVIII'}
+    ${39}   | ${'XXXIX'}
+    ${40}   | ${'XL'}
+    ${41}   | ${'XLI'}
+    ${42}   | ${'XLII'}
+    ${43}   | ${'XLIII'}
+    ${44}   | ${'XLIV'}
+    ${45}   | ${'XLV'}
+    ${46}   | ${'XLVI'}
+    ${47}   | ${'XLVII'}
+    ${48}   | ${'XLVIII'}
+    ${49}   | ${'XLIX'}
+    ${50}   | ${'L'}
+    ${87}   | ${'LXXXVII'}
+    ${90}   | ${'XC'}
+    ${91}   | ${'XCI'}
+    ${100}  | ${'C'}
+    ${239}  | ${'CCXXXIX'}
+    ${400}  | ${'CD'}
+    ${444}  | ${'CDXLIV'}
+    ${500}  | ${'D'}
+    ${692}  | ${'DCXCII'}
+    ${900}  | ${'CM'}
+    ${999}  | ${'CMXCIX'}
+    ${1000} | ${'M'}
+    ${2016} | ${'MMXVI'}
+  `('should convert $input to $expected', ({ input, expected }) => {
+    const result = toRoman(input);
+    expect(result).toEqual(expected);
   });
 });
